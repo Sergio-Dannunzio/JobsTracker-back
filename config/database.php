eb0b3e208ec6584ad6dd8484a897c933c1904b56
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-23 09:01:38 -0300

    fix connection with .env

--- config/database.php
+++ config/database.php
@@ -1,10 +1,21 @@
 <?php
-require 'vendor/autoload.php'; //carga dependencias de composer
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$mongoClient = new MongoDB\Client("mongodb+srv://sergiovdannunzio:V6N22YjV4ehWbM7A@jobscluster.pgjl7.mongodb.net/?retryWrites=true&w=majority&appName=JobsCluster");
+// Cargar variables de entorno
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-$database = $mongoClient->selectDatabase("miDB");
-$collection = $database->selectCollection("users");
+// Obtener la URI de MongoDB y el nombre de la base de datos
+$mongoUri = $_ENV['MONGO_URI'] ?? null;
+$databaseName = $_ENV['DB_NAME'] ?? null;
+$collectionName = $_ENV['COLLECTION_NAME'] ?? null;
 
-echo "Conexión exitosa a MongoDB!";
+if (!$mongoUri || !$databaseName || !$collectionName) {
+    die('La configuración de la base de datos es incorrecta.');
+}
+
+// Crear el cliente y seleccionar la base de datos y colección
+$client = new MongoDB\Client($mongoUri);
+$database = $client->selectDatabase($databaseName);
+$collection = $database->selectCollection($collectionName);
 ?>
